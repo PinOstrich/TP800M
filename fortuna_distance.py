7b4a05b269dfb2a986d6f2386b7635037e16f368
aris <aristidis.akritidis@gmail.com>
2025-09-07 15:30:41 +0200

    Added distance data and assumed function shape

--- fortuna_distance.py
+++ fortuna_distance.py
@@ -2,10 +2,11 @@ import numpy as np
 import random as rn
 import json
 import math
+import matplotlib.pyplot as plt
 
 xp,yp = [],[]
 
-with open("some_data_for_class.txt", "r") as f:
+with open("data.txt", "r") as f:
     for line in f:
         record = json.loads(line)
         xp.append(record["x"])
@@ -15,10 +16,8 @@ def f(theta, x):
     s =  theta[0]
     t1 =  theta[1]
     t2 =  theta[2]
-    t3 =  theta[3]
-    t4 =  theta[4]
-
-    val = ((x[0]**0.5)*t1 + (x[1]**0.5)*t2 + (x[2]**0.1)*5 + s) / t4
+    
+    val = x**s 
     return val
 
 def error(y_, y):
@@ -26,29 +25,31 @@ def error(y_, y):
     
 
 best_loss = float('inf')
-best_model = [0,0,0,0,0]
+best_model = [0,0,0]
 
 for i in range(10000):
-    s =  rn.randint(1,80)
+    s =  rn.randint(0,3)
     t1 =  rn.randint(-100,100)
     t2 =  rn.randint(-50,-1)
-    t3 =  rn.randint(-10,10)
-    t4 = rn.randint(1,10)
     loss = 0
 
     for x, y in zip(xp, yp):
-        theta = [s,t1,t2,t3,t4]
+        theta = [s,t1,t2]
         yhat = f(theta,x)
         loss += error(yhat,y)
     
     if(loss<best_loss):
         best_loss = loss
-        best_model = [s,t1,t2,t3,t4]
+        best_model = [s,t1,t2]
         print("new best loss: ",best_loss)
+
+plt.plot(xp, yp)
+plt.xlabel("data")
+plt.ylabel("distance")
+plt.show()
 
 print(best_loss)
 print("s: " + str(best_model[0]))
 print("t1: " + str(best_model[1]))
 print("t2: " + str(best_model[2]))
-print("t3: " + str(best_model[3]))
-print("t4: " + str(best_model[4]))
+
